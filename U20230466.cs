160afb0229e3f4dfeb9c06800c5eec152a5e9ce4
Carlos <jccp503@gmail.com>
2023-08-02 14:58:20 -0600

    Declaracion de variable

---
+++ U20230466.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DECLARACION DE VARIABLES
+
+            //ITEMS 1
+
+            int edad = 20;
+
+            string objeto = "computadora";
+
+            float salario = 502.25;
+
+            decimal precio = 20.3658;
+
+            bool activo = true;
+
+            
+
+
+            //AUTOEVALUACION 10
+        }
+        
+
+
+
+
+    }
+}
