1443ed8036765fd562047009bd96cca760fe8084
Carlos <jccp503@gmail.com>
2023-08-02 15:25:18 -0600

    Declaración de Tipos de Variables

--- U20230466.cs
+++ U20230466.cs
@@ -10,7 +10,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             //DECLARACION DE VARIABLES
 
-            //ITEMS 1
+
 
             int edad = 20;
 
@@ -22,8 +22,29 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             bool activo = true;
 
-            
+            //ITEMS 1 DECLARACION DE VARIABLE DE TIPO STRING
 
+            string primernombre = "Juan"; 
+            string segundonombre = "Carlos";
+            string primerapellido = "Castillo";
+            string segundoapellido = "Parada";
+
+            //ITEMS 2 DECLARACION DE VARIABLE DE TIPO INT
+
+            int edadCarlos = 20;
+            int edadPadre = 69;
+            int edadMadre = 64;
+
+            //ITEMS 3 DECCLARACION DE VARIABLE DE TIPO FLOAT
+
+            float internet = 30.00;
+            float transporte = 3.75;
+            float almuerzo = 5.50;
+
+            //ITEMS 4 DECLARACION DE VARIABLE DE TIPO BOOLEANO
+ 
+            bool estudiante = true;
+            bool becado = true;
 
             //AUTOEVALUACION 10
         }
